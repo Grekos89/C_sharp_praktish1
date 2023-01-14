3d6ee87d9e871c7d59dcd16c0a4e12f7a10fd7da
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-14 20:47:20 +0300

    example2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿int number1 = Convert.ToInt32(Console.ReadLine());
+﻿//int number1 = Convert.ToInt32(Console.ReadLine());
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if(number1 > number2)
+//{
+//    System.Console.WriteLine(number1);
+//}
+//else
+//{
+//    System.Console.WriteLine(number2);
+//}
+
+
+//задача 2
+
+int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
-if(number1 > number2)
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if( number1 > number2)
 {
-    System.Console.WriteLine(number1);
+     max = number1; 
 }
 else
 {
-    System.Console.WriteLine(number2);
+    max = number2;
+}
+if(max > number3)
+{
+    System.Console.WriteLine(max);
+}
+else
+{
+    System.Console.WriteLine(number3);
 }
