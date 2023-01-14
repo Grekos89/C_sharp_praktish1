1b42e76941a7612026ddc2130fa256189871c6be
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-14 20:19:02 +0300

    example1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    System.Console.WriteLine(number1);
+}
+else
+{
+    System.Console.WriteLine(number2);
+}
